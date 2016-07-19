e3f4579ecdd5563ce9eb5d107315e411c90ddd71
Teerasak Vichadee <teerasak.vic@ascendcorp.com>
2016-07-19 21:11:09 +0700

    ADD greeting class

---
+++ src/test/java/hello/GreetingTest.java
@@ -0,0 +1,19 @@
+package hello;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class GreetingTest {
+
+    @Test
+    public void getId() throws Exception {
+
+    }
+
+    @Test
+    public void getName() throws Exception {
+
+    }
+
+}
